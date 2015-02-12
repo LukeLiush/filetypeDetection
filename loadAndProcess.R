66230d8ce361ad1d6da26738ba2f723c2fae2ca9
LukeLiush <hanson311biz@gmail.com>
2015-02-12 02:59:25 -0800

    Update loadAndProcess.R

--- loadAndProcess.R
+++ loadAndProcess.R
@@ -7,15 +7,15 @@ loadAndProcess<-function(tnpath, vpath, tspath){
 	m<-dim(train)[1]
 	n<-dim(train)[2]
 
-	X<-train[,1:n-1]
+	X<-train[,1:n-1]^0.5
 	X<-cbind(numeric(m)+1, X) # add all 1 in column 1
 	y<-train[,n]
 
-	Xval<-val[,1:n-1]
+	Xval<-val[,1:n-1]^0.5
 	Xval<-cbind(numeric(dim(Xval)[1])+1, Xval) # add all 1 in column 1
 	yval<-val[,n]
 
-	Xtest<-test[,1:n-1]
+	Xtest<-test[,1:n-1]^0.5
 	Xtest<-cbind(numeric(dim(Xtest)[1])+1, Xtest) # add all 1 in column 1
 	ytest<-test[,n]
 
